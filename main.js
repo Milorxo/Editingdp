6c03d7256cdc7e919811afd15b25f29b637bfe01
Milorxo <118413116+Milorxo@users.noreply.github.com>
2025-08-22 16:57:34 +0700

    Update main.js

--- main.js
+++ main.js
@@ -7,13 +7,19 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
+    icon: __dirname + 'pd.ico',  // path to your icon
     title: "Daily Productivity"
+    frame: true,
+    autoHideMenuBar: true
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false
     }
   });
+// Remove the menu bar completely
+mainWindow.setMenuBarVisibility(false);
 
+mainWindow.loadFile('index.html');
   // Load your web app (local index.html)
   win.loadFile(path.join(__dirname, 'index.html'));
 
